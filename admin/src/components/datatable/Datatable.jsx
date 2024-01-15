33e9932cc06fac095667d986ad4472d24eced50c
hieudeptraihihi <52031694+hieudeptraihihi@users.noreply.github.com>
2024-01-15 08:20:16 +0700

    update error white page

--- admin/src/components/datatable/Datatable.jsx
+++ admin/src/components/datatable/Datatable.jsx
@@ -58,7 +58,7 @@ const Datatable = ({columns}) => {
       </div>
       <DataGrid
         className="datagrid"
-        rows={list}
+        rows={list || []}
         columns={columns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
